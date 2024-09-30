9c3e84628a2045a5bff629688c4950cb702eda64
Ferdinandvich <ftellmann@mail.com>
2024-09-30 11:42:56 -0300

    Add files via upload

---
+++ aula9/notas.py
@@ -0,0 +1,44 @@
+"""
+Pergunta nomes e notas e publica no final.
+"""
+resposta = int(input("Quantos alunos para calcular? "))
+
+i = 0
+alunos = []
+
+while i < resposta: 
+
+    nomeAluno = input("Digite seu nome : ")
+    nota1 = float(input("Digite a primeira nota : "))
+    nota2 = float(input("Digite a segunda nota : "))
+    media = (nota1 + nota2)/2
+    resultado = "Aprovado" if media >= 6.5 else "Reprovado"
+
+    alunos.append(
+        {
+            "nome": nomeAluno,
+            "nota1": nota1,
+            "nota2": nota2,
+            "media": media,
+            "resultado": resultado
+
+        }
+    
+    )
+
+    i = i + 1
+
+for aluno in alunos:
+    print(aluno["nome"], " (Media: ", aluno["media"], "-",aluno["resultado"]," )")
+
+
+
+
+
+# if(media >= 6.5):
+   # print(f"{media} - Aprovado!" ) 
+#else: 
+   # print(f"{media} - Reprovado!" )
+
+    
+
